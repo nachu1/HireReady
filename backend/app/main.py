851f78c0968a77e35124b165c4a912d769e60904
nachu1 <nasarc530@gmail.com>
2026-06-20 16:22:08 +0530

    fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://hireready-frontend-99jn.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
